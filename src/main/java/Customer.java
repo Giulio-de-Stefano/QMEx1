f3e0011316e0573961cdeeff77a1b714bc61c808
Giulio de Stefano <padremaronno89@gmail.com>
2017-06-11 16:28:35 +0100

    Added simple implementation of a Customer class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,19 @@
+public class Customer {
+
+    public static int global_id = 0;
+    private final int id;
+    private final Cart cart;
+
+    public Customer() {
+        this.id = global_id++;
+        this.cart = new Cart();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+}
