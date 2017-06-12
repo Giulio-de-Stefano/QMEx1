67d93d4c23f04122cf244a2cd7d81650bd14f3ab
giulio.destefano <giulio.destefano@gamesys.co.uk>
2017-06-12 17:26:23 +0100

    Overrided equals and hashcode for Product

--- src/main/java/market/Product.java
+++ src/main/java/market/Product.java
@@ -42,4 +42,23 @@ public class Product {
     public boolean isCountable() {
         return isCountable;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (isCountable != product.isCountable) return false;
+        return name.equals(product.name) && price.equals(product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + price.hashCode();
+        result = 31 * result + (isCountable ? 1 : 0);
+        return result;
+    }
 }
